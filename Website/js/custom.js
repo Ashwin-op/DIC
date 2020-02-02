b1af9edc05d14bc044b71c3bc3064cb12feb5551
Ashwin-op <60032753+Ashwin-op@users.noreply.github.com>
2020-02-02 08:59:01 +0530

    Initial
    Update #1

---
+++ Website/js/custom.js
@@ -0,0 +1,62 @@
+;(function($){
+    "use strict"
+    var nav_offset_top = $('.header_area').height()+50; 
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+    
+    function testimonialSlider(){
+        if ( $('.testimonial_slider').length ){
+            $('.testimonial_slider').owlCarousel({
+                loop:true,
+                margin: 30,
+                items: 2,
+                nav:false,
+                autoplay: true,
+                dots: true,
+                smartSpeed: 1500,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    768: {
+                        items: 2,
+                    },
+                }
+            })
+        }
+    }
+    testimonialSlider();
+    
+    //------- Mailchimp js --------//  
+
+    function mailChimp(){
+        $('#mc_embed_signup').find('form').ajaxChimp();
+    }
+    mailChimp();
+    
+    /* ===== Parallax Effect===== */
+	
+	function parallaxEffect() {
+    	$('.bg-parallax').parallax();
+	}
+	parallaxEffect();
+    
+
+})(jQuery)
